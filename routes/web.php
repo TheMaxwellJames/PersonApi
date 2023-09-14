a0f5f9c21586c9d4b85b31a2cdeef3e1f5274729
Maxwell James <maxichanj@gmail.com>
2023-09-14 17:39:43 +0100

    remove control

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 
-use App\Http\Controllers\HomeController;
-
-use App\Http\Controllers\Backend\AuthController;
-
-use App\Http\Controllers\Backend\DashboardController;
-
-use App\Http\Controllers\Backend\PortfolioController;
-
-use App\Http\Controllers\Backend\BlogController;
-
-use App\Http\Controllers\Backend\SocialIconController;
-
-use App\Http\Controllers\Backend\MyAccountController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,28 +14,11 @@ use App\Http\Controllers\Backend\MyAccountController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
 
 
-Route::get('/', [HomeController::class, 'index']);
 
-Route::get('about', [HomeController::class, 'about']);
-
-
-Route::get('portfolio', [HomeController::class, 'portfolio']);
-
-Route::get('contact', [HomeController::class, 'contact']);
-
-Route::get('blog', [HomeController::class, 'blog']);
-
-Route::get('newsletter', [HomeController::class, 'newsletter']);
-
-
-Route::get('blog-post/{id}', [HomeController::class, 'blog_post']);
-
-
-Route::post('contact/post', [HomeController::class, 'contact_post']);
 
